602b68fafffed5af7c0849b4400cccf2130705ed
Hanan Marghoob <107300415+Human-100@users.noreply.github.com>
2025-01-29 22:08:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(API_KEY);
-console.log(API_KEY)
+// console.log(API_KEY)
 
 async function run(request) {
   // For text-only input, use the gemini-pro model
